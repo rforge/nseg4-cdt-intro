b3d417474d2257ca8d02f28ea2e6fd6c6cc6e362
sarahheaps <sarahheaps@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-09-19 16:15:23 +0000

    Added source.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nseg4-cdt-intro@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/cdtRintroduction-package.R
@@ -0,0 +1,11 @@
+#' R courses for Newcastle University's CDT in Cloud 
+#' Computing for Big Data
+#' 
+#' Functions and datasets used for Newcastle University's 
+#' CDT in Cloud Computing for Big Data. This package is 
+#' used in the "Introduction to R" course.
+#' @name cdtRintroduction-package
+#' @aliases cdtRintroduction cdtrintroduction
+#' @docType package
+#' @keywords package
+NULL
